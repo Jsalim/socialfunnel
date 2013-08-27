77a4e7be1c16edca5266e9782df98e818b590c94
Misael Neto <misawsneto@gmail.com>
2013-08-27 03:19:31 -0300

    alguns ajustes

--- public/js/ajaxscript.js
+++ public/js/ajaxscript.js
@@ -2,7 +2,7 @@
 	$("a").on("click", function(e) {
 		var source = $(this).attr("data-source");
 		var target = $(this).attr("data-target");
-		
+		console.log(source + " " + target);
 		if (source == null)
 			source = $(this).attr("href");
 		if (source != null && target != null) {
@@ -12,6 +12,9 @@
 				url : source,
 				success : function(response) {
 					$('div[data-id="' + target + '"]').html(response);
+					
+					console.log(source + " " + target);
+					
 					$("#content").scrollTop(0)
 				}
 			});
