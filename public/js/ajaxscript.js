19449210c7eca216ce9d2b420a86bba3e181ac7f
Misael Neto <misawsneto@gmail.com>
2013-09-25 22:58:14 -0300

    ajustes

--- public/js/ajaxscript.js
+++ public/js/ajaxscript.js
@@ -8,35 +8,39 @@ function helpdeskTabSwitch(){
 }
 
 (function($) {
-	$("a").on("click", function(e) {
-		var source = $(this).attr("data-source");
-		var target = $(this).attr("data-target");
-		if (source == null)
-			source = $(this).attr("href");
-		if (source != null && target != null) {
-			e.preventDefault();
-			$.ajax({
-				type : "GET",
-				url : source,
-				success : function(response) {
-					$('div[data-id="' + target + '"]').html(response);
-					
-					$(document).scrollTop(0)
-				}
-			});
-		}
-	});
 	
-	$("li.tab").on('click', function(){
-		$('div#sidebar-nav div.pointer').remove();
-		$("li.tab").removeClass("active");
-		$(this).addClass("active");
-		$(this).prepend(
-			'<div class="pointer">' +
-                '<div class="arrow"></div>' +
-                '<div class="arrow_border"></div>' +
-            '</div>'	
-		);
+	$("#sidebar-nav li").on("click", function(e){
+		var tab = ($(this));
+		var anchor = ($(this).children("a"));
+		
+		if(anchor){
+			var source = $(anchor).attr("data-source");
+			var target = $(anchor).attr("data-target");
+			if (source == null)
+				source = $(anchor).attr("href");
+			if (source != null && target != null) {
+				e.preventDefault();
+				$.ajax({
+					type : "GET",
+					url : source,
+					success : function(response) {
+						$('div[data-id="' + target + '"]').html(response);
+						
+						$('div#sidebar-nav div.pointer').remove();
+						$("li.tab").removeClass("active");
+						$(tab).addClass("active");
+						$(tab).prepend(
+							'<div class="pointer">' +
+				                '<div class="arrow"></div>' +
+				                '<div class="arrow_border"></div>' +
+				            '</div>'	
+						);
+						
+						$(document).scrollTop(0)
+					}
+				});
+			}
+		}
 	});
 	
 	helpdeskTabSwitch();
